8da7e17589777680752319edd926f86dc037d53d
paulo-ciriaco <paulo.e.santos14@aluno.senai.br>
2026-09-04 10:09:00 -0300

    feat: implementa GET de carteiras

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/CarteiraController.cs
@@ -0,0 +1,18 @@
+﻿using BancoSENAIAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BancoSENAIAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CarteiraController : ControllerBase
+    {
+        private static List<Carteira> _carteiras = new List<Carteira>();
+
+        [HttpGet]
+        public IActionResult ListarTodas()
+        {
+            return Ok(_carteiras);
+        }
+    }
+}
