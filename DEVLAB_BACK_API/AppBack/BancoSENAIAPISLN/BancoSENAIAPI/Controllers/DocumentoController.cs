3cf54bd2dbc63ed7cb3fad877a51ad427d8a0073
paulo-ciriaco <paulo.e.santos14@aluno.senai.br>
2026-09-11 08:49:15 -0300

    feat(documentos): inicio upload endpoint

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -3,7 +3,8 @@
 namespace BancoSENAIAPI.Controllers
 {
     [ApiController]
-    [Route("api/v1[controller]")]
+    [Route("api/v1/[controller]")]
+
     public class DocumentoController : Controller
     {
         private readonly string _caminhoRaiz = Path.Combine(
@@ -13,5 +14,11 @@ namespace BancoSENAIAPI.Controllers
         private static List<Models.DocumentoMetadado> _documentosMetadados = new List<Models.DocumentoMetadado>();
 
         private static int _nextId = 1;
+
+        [HttpPost("upload/{codigoCliente}")]
+        public async Task<IActionResult> AnexarArquivo(int codigoCliente, IFormFile arquivo)
+        {
+
+        }
     }
 }
