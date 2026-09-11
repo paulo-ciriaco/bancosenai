699cb61bbbb22d947c773bf39cde8c1dcc065d8a
paulo-ciriaco <paulo.e.santos14@aluno.senai.br>
2026-09-11 09:19:17 -0300

    feat(documento): salvando metadado do upload

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -39,6 +39,19 @@ namespace BancoSENAIAPI.Controllers
             {
                 await arquivo.CopyToAsync(stream);
             }
+
+            var documentoMetadados = new Models.DocumentoMetadado
+            {
+                Id = _nextId++,
+                Name = nomeOriginal,
+                Extensao = extensao,
+                Caminho = caminhoFinal,
+                CodigoCliente = codigoCliente
+            };
+
+            _documentosMetadados.Add(documentoMetadados);
+
+            return OK(new { mensagem = "Documento anexado com sucesso", arquivoSalvo = novoNome });
         }   
     }
 }
